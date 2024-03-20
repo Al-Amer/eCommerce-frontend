ff4100f05fc85fc4a9c23ec690f6bd1b0232717b
ameralmunajjed <a.munajed@icloud.com>
2024-03-20 22:40:32 +0100

    this is the first step in project building

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Product } from '../../common/product';
+import { ProductService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-table.component.html',
+//  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit {
+
+  produts: Product[]= [];
+  constructor(private ProductService:ProductService) {}
+
+  ngOnInit(): void {
+    this.listProduts();
+  }
+
+  listProduts(){
+    this.ProductService.getProductList().subscribe(
+      data=> {this.produts = data;
+      }
+    );
+  }
+
+
+}
